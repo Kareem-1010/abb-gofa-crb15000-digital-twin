2bede4d8faf0d56aa6b57a537368e0a67d27b27a
Kareem-1010 <kareemun1005@gmail.com>
2025-11-30 14:43:44 +0530

    Initial commit: Add Unity project files

---
+++ Assets/Script/Gabriella_Experiments/PIckUpTask.cs
@@ -0,0 +1,118 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PIckUpTask : MonoBehaviour
+{
+    public GameObject cubeA;
+    public GameObject cubeB;
+    private Rigidbody cubeRigidbody;
+    private float distanceToCubeB;
+    public bool isHoldingCube = false;
+    public bool canPickup = false;
+    private Vector3 collisionOffset;
+    private Transform originalParent;
+    public TMP_Text distanceText;
+    public Button toggleDistanceButton;
+    private bool showDistance = false;
+
+    private void Start()
+    {
+        cubeRigidbody = cubeA.GetComponent<Rigidbody>();
+        originalParent = cubeA.transform.parent;
+        distanceText.text = "";
+        showDistance = false;
+        if (toggleDistanceButton != null)
+        {
+            toggleDistanceButton.onClick.AddListener(ToggleDistanceText);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!isHoldingCube && canPickup)
+            {
+                AttachCube();
+            }
+            else if (isHoldingCube)
+            {
+                DetachCube();
+            }
+            if (showDistance && distanceText.text != "")
+            {
+                CheckDistance();
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == cubeA)
+        {
+            canPickup = true;
+            ContactPoint contact = collision.contacts[0];
+            collisionOffset = transform.InverseTransformPoint(contact.point);
+            Debug.Log("cubeA is in range for pickup");
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject == cubeA)
+        {
+            canPickup = false;
+        }
+    }
+
+    public void AttachCube()
+    {
+        //Modification by Teemu K
+        if (!canPickup) return;
+
+        Debug.Log("Picking up cubeA");
+        cubeRigidbody.isKinematic = true;
+        cubeA.transform.SetParent(transform);
+        cubeA.transform.localPosition = collisionOffset;
+        isHoldingCube = true;
+        canPickup = true;
+    }
+
+    public void DetachCube()
+    {
+        cubeA.transform.SetParent(originalParent != null ? originalParent : null);
+        cubeRigidbody.isKinematic = false;
+        cubeRigidbody.linearVelocity = Vector3.zero;
+        cubeRigidbody.angularVelocity = Vector3.zero;
+        isHoldingCube = false;
+        canPickup = false;
+        Debug.Log("cubeA detached");
+    }
+
+    private void CheckDistance()
+    {
+        if (cubeA != null && cubeB != null)
+        {
+            distanceToCubeB = Vector3.Distance(cubeA.transform.position, cubeB.transform.position);
+
+            float distanceInCm = distanceToCubeB * 100f;
+
+            distanceText.text = $"Distance to target: {distanceInCm:F2} cm";
+        }
+    }
+
+    private void ToggleDistanceText()
+    {
+        showDistance = !showDistance;
+
+        if (showDistance)
+        {
+            CheckDistance();
+        }
+        else
+        {
+            distanceText.text = "";  // Clear the text
+        }
+    }
+}
